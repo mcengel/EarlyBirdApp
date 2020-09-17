7d0c6f2eb00ec97a092807574ff61c3265dbf238
Tobias Mauritz <tobias.mauritz@stud.uni-due.de>
2020-09-17 09:21:22 +0200

    added Folders for structure + views for hierarchical navigation example

---
+++ EarlyBirdApp/EarlyBirdApp/Views/StartGame3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EarlyBirdApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartGame3 : ContentPage
+    {
+        public StartGame3()
+        {
+            InitializeComponent();
+        }
+        protected async void GoToPreviousPage(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+        protected async void GoToStartPage(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
