d6eefe6dfc5921c04265c05532d9a28e5cdfb553
Marek Engel <marek.engel96@outlook.de>
2020-09-17 11:59:14 +0200

    Change UI and added button

--- EarlyBirdApp/EarlyBirdApp/MainPage.xaml.cs
+++ EarlyBirdApp/EarlyBirdApp/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,31 @@ namespace EarlyBirdApp
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
+            
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("&&&&&"+ Path.GetFullPath(Environment.CurrentDirectory));
+                File.Create(Path.GetFullPath(Environment.CurrentDirectory));
+                string path = Path.Combine(Path.GetTempPath(), @"persitence", "TextFile1.txt");
+                Console.WriteLine("$$$$$" + Environment.CurrentDirectory);
+                Console.WriteLine("§§§§§" + path);
+                string[] file = System.IO.File.ReadAllLines(path);
+                Console.WriteLine("?????" + file[0]);
+
+            }catch(Exception errorMsg)
+            {
+                Console.WriteLine(errorMsg.Message);
+            }
         }
     }
+
 }
