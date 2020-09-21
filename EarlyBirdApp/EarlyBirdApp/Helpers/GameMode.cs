99061d21467820d26ebe7b385f7908bd1c81e893
Tobias Mauritz <tobias.mauritz@stud.uni-due.de>
2020-09-21 06:58:43 +0200

    added GameMode to Helper

---
+++ EarlyBirdApp/EarlyBirdApp/Helpers/GameMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarlyBirdApp.Helpers
+{
+    public static class GameMode
+    {
+        public static List<string> RandomizeGames(List<string> selectedGames)
+        {
+            return null;
+        }
+
+        public static List<string> RandomizeGames()
+        {
+            return null;
+        }
+    }
+}
